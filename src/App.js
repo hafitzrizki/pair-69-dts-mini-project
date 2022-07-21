7313535bcc056aee7f51dd855d824063ed894729
hafitzrizki <hafitzrizki@gmail.com>
2022-07-21 23:26:07 +0700

    update text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Movies Web App &#127871;
         </p>
         <a
           className="App-link"
