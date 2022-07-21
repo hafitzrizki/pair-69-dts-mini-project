a495be5318f72200968abbb2678794844f1cb7fd
hafitzrizki <hafitzrizki@gmail.com>
2022-07-21 23:35:54 +0700

    update link

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/hafitzrizki/pair-69-dts-mini-project"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link repo
         </a>
       </header>
     </div>
